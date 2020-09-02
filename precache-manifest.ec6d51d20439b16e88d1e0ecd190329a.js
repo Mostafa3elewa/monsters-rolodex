68a4ea7677a97749172ba9262c8804eb7ebf382c
mostafa3elewa <mostafa.ahmed.elewa@gmail.com>
2020-09-02 15:36:48 +0200

    Updates

---
+++ precache-manifest.ec6d51d20439b16e88d1e0ecd190329a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33c5b82dabb9a4fb4e39e7884255370c",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "4f8c2cbdadc35ed44073",
+    "url": "/monsters-rolodex/static/css/main.65bd9042.chunk.css"
+  },
+  {
+    "revision": "78adac64eaa07ff35dea",
+    "url": "/monsters-rolodex/static/js/2.356d906f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.356d906f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f8c2cbdadc35ed44073",
+    "url": "/monsters-rolodex/static/js/main.fef10b6d.chunk.js"
+  },
+  {
+    "revision": "f553da120d846b640e5e",
+    "url": "/monsters-rolodex/static/js/runtime-main.1dfd2aab.js"
+  }
+]);
